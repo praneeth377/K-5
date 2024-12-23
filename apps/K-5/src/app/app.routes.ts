a368df05fa0f7b2ab2f4c97878d753559b2993fb
sushmi-08 <sush.08.ys@gmail.com>
2024-12-23 11:37:17 +0530

    design changes

--- apps/K-5/src/app/app.routes.ts
+++ apps/K-5/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Route } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { ChaptersComponent } from './chapters/chapters.component';
+import { LandingPageComponent } from './landing/landing_page.component';
 
 export const appRoutes: Route[] = [
     { path: 'courses', component: CoursesComponent },
     { path: 'courses/:courseId/chapters', component: ChaptersComponent },
+    { path: 'landing', component: LandingPageComponent}
 ];
--- apps/K-5/src/app/landing/landing_page.component.ts
+++ apps/K-5/src/app/landing/landing_page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
+import { CoursesComponent } from '../courses/courses.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, MatProgressBarModule],
+  imports: [CommonModule, MatProgressBarModule, MatButtonModule, CoursesComponent],
   templateUrl: './landing_page.component.html',
   styleUrl: './landing_page.component.css',
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  constructor(private router:Router){}
+  
+  buttonClick():void {
+    this.router.navigate(['/courses']);
+  }
+}
